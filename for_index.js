71eb91f7d1cf26dc75501814e6c73788b71d93ca
lartik <36516154+LaRtik@users.noreply.github.com>
2023-04-12 23:07:48 +0300

    added first prototype, some adaptive on main page

---
+++ for_index.js
@@ -0,0 +1,24 @@
+
+const menubtn = document.querySelector('.menu-btn');
+const closemenubtn = document.querySelector('.close-menu-btn');
+const navbar = document.getElementById("nav-bar");
+
+
+menubtn.addEventListener("click", function () {
+    if (menubtn.className !== "")
+    {
+        menubtn.style.display = "none";
+        closemenubtn.style.display = "block";
+        navbar.classList.add("show-nav");
+    }
+})
+
+
+closemenubtn.addEventListener("click", function () {
+    if (closemenubtn.className !== "")
+    {
+        closemenubtn.style.display = "none";
+        menubtn.style.display = "block";
+        navbar.classList.remove("show-nav");
+    }
+})
