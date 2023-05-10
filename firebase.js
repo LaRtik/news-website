393ec241f6792a4993a0ab0aced11a3d3d75d503
lartik <36516154+LaRtik@users.noreply.github.com>
2023-05-10 22:37:58 +0300

    added login via firebase/auth (to be replaced with firestore db), started connecting admin panel to db

--- firebase.js
+++ firebase.js
@@ -11,12 +11,18 @@ const firebaseConfig = {
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 const db = firebaseApp.firestore();
 const auth = firebaseApp.auth();
+auth.setPersistence(firebase.auth.Auth.Persistence.SESSION); 
 
 
 var createArticle = document.getElementById('create-article-form');
 var createArticleText = document.getElementById('html-output');
 var createArticleTitle = document.getElementById('topic-selector');
 var createArticleButton = document.getElementById('submit-button');
+var loginForm = document.getElementById('login-form');
+var loginText = document.getElementById('login');
+var passwordText = document.getElementById('password');
+var errorField = document.getElementById('error-field');
+var adminTableBody = document.getElementById('admin-table-body');
 
 function writeNewArticle(title, body) {
 	var postData = {
@@ -29,18 +35,86 @@ function writeNewArticle(title, body) {
 		alert("Post was succesfully posted!");
 		window.location.reload();
 	})
-	.catch((error) => {
-		console.error("Error adding post: ", error);
-	});
+		.catch((error) => {
+			console.error("Error adding post: ", error);
+		});
 }
 
-createArticle.onsubmit = function (e) {
+if (createArticle) {
+	createArticle.onsubmit = function (e) {
 	e.preventDefault();
 	var text = createArticleText.value;
-	var title = createArticleTitle.value;
+	var title = createArticleTitle.value;	
 	if (title.length > 80) {
 		alert("Title length can not more then 80 symbols");
 		return;
 	}
 	writeNewArticle(title, text);
 };
+}
+
+
+if (loginForm) {
+	if (localStorage.getItem("user")) {	
+		errorField.innerHTML = "Now logged as " + localStorage.getItem("user");
+	}
+	
+	loginForm.onsubmit = function (e) {
+		e.preventDefault();
+		var login = loginText.value;
+		var password = passwordText.value;
+		if (db.collection('users').where("login", "==", login)) {
+			if (db.collection('users').where("password", "==", password)) {
+
+			}
+		}
+
+		//db.collection('posts').add(postData).then((docRef) => {
+		//	console.log('Post added with ID: ', docRef.id);
+		//	alert("Post was succesfully posted!");
+		//	window.location.reload();
+		//})
+		//	.catch((error) => {
+		//		console.error("Error adding post: ", error);
+		//	});
+		auth.signInWithEmailAndPassword(login, password)
+			.then((userCredential) => {
+				// Signed in
+				user = userCredential.user;
+				errorField.innerHTML = "Now logged as " + auth.currentUser.email;
+				localStorage.setItem("user", user.email);
+				window.location.href = "create.html";
+				// ...
+			})
+			.catch((error) => {
+				var errorCode = error.code;
+				var errorMessage = error.message;
+				alert(errorMessage);
+				errorField.innerHTML = error;
+			});
+	};
+}
+
+if (adminTableBody) {
+	const listAllUsers = (nextPageToken) => {
+		auth
+		.listUsers(1000, nextPageToken)
+		.then((listUsersResult) => {
+			listUsersResult.users.forEach((userRecord) => {
+				console.log('user', userRecord.toJSON());
+			})
+			if (listUsersResult.pageToken) {
+				listAllUsers(listUsersResult.pageToken)
+			}
+		})
+		.catch((error) => {
+			console.log("Error listening users: ", error);
+		});
+	}
+	listAllUsers();
+}
+
+
+
+
+
